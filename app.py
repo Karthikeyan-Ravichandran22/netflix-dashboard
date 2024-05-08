569654ca718ea8804448af0d0a6fd6daa80eb7f6
Karthikeyan-Ravichandran22 <karthikeyan.ravichandran22@gmail.com>
2024-05-08 22:06:20 +0100

    Updated file

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-<<<<<<< HEAD
 # Load dataset using file uploader
 def load_data_via_uploader():
     uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
     if uploaded_file is not None:
         return pd.read_csv(uploaded_file)
     else:
-=======
-# Load dataset
-def load_data():
-    try:
-        return pd.read_csv(r'data/simple_netflix.csv')
-    except Exception as e:
-        st.error(f"Error loading dataset: {e}")
->>>>>>> 724c6ddc5747acc207e90e0cb21a0941f9c7d462
         return pd.DataFrame()
 
 # Load data
